f234ab5842555090cc02567fe1d44734d76d6bbe
Mejbaur Bahar Fagun <fagun115946@gmail.com>
2023-08-08 22:09:06 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,198 +1,133 @@
+import os
+import re
+import time
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from difflib import HtmlDiff
-import re
 from urllib.parse import urljoin
-import sys
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from PIL import Image, ImageChops
 
 
-def validate_url(url):
-    pattern = re.compile(
-        r"^(https?://)?"
-        r"((([a-zA-Z0-9_-]+)\.)+[a-zA-Z]{2,})"
-        r"(/([a-zA-Z0-9_./-]+)*)?$"
-    )
-    return bool(re.match(pattern, url))
+class WebsiteComparer:
+    def __init__(self):
+        self.options = webdriver.FirefoxOptions()
+        self.options.add_argument("--headless")
 
+    def validate_url(self, url):
+        pattern = re.compile(
+            r"^(https?://)?"
+            r"((([a-zA-Z0-9_-]+)\.)+[a-zA-Z]{2,})"
+            r"(/([a-zA-Z0-9_./-]+)*)?$"
+        )
+        return bool(re.match(pattern, url))
 
-def get_full_page_screenshot(driver, file_path):
-    original_size = driver.get_window_size()
-    required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
-    required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-    driver.set_window_size(required_width, required_height)
-    driver.save_screenshot(file_path)
-    driver.set_window_size(original_size['width'], original_size['height'])
+    def get_page_urls(self, url):
+        response = requests.get(url)
 
+        if response.status_code != 200:
+            print("Error: Website could not be accessed.")
+            return []
 
-def get_page_urls(url):
-    response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        links = soup.find_all("a")
 
-    if response.status_code != 200:
-        print("Error: Website could not be accessed.")
-        return []
+        page_urls = []
+        for link in links:
+            href = link.get("href")
+            if href and not href.startswith("#"):
+                page_url = urljoin(url, href)
+                if page_url not in page_urls:
+                    page_urls.append(page_url)
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    links = soup.find_all("a")
+        return page_urls
 
-    page_urls = []
-    for link in links:
-        href = link.get("href")
-        if href and not href.startswith("#"):
-            page_url = urljoin(url, href)
-            if page_url not in page_urls:
-                page_urls.append(page_url)
+    def check_broken_links(self, driver):
+        links = driver.find_elements_by_tag_name("a")
+        broken_links = []
+        for link in links:
+            href = link.get_attribute("href")
+            if href:
+                response = requests.head(href)
+                if response.status_code != 200:
+                    broken_links.append(href)
+        return broken_links
 
-    return page_urls
+    def analyze_page_load_times(self, driver, url):
+        driver.get(url)
+        navigation_start = driver.execute_script("return window.performance.timing.navigationStart")
+        load_event_end = driver.execute_script("return window.performance.timing.loadEventEnd")
+        page_load_time = (load_event_end - navigation_start) / 1000
+        print(f"Page load time for {url}: {page_load_time:.2f} seconds")
 
+    def compare_websites(self, url1, url2):
+        if not self.validate_url(url1) or not self.validate_url(url2):
+            print("Invalid URL(s).")
+            return
 
-def save_page_urls(url, page_urls, filename):
-    with open(filename, "w") as file:
-        file.write("Page URLs for " + url + ":\n")
-        for page_url in page_urls:
-            file.write(page_url + "\n")
-    print("Page URLs saved to " + filename)
+        if url1 == url2:
+            print("Please enter two different URLs for website 1 and website 2.")
+            return
 
+        driver1 = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=self.options)
+        driver2 = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=self.options)
 
-def compare_page_urls(file1, file2):
-    with open(file1, "r") as file:
-        urls1 = set(line.strip() for line in file.readlines())
+        # Compare broken links
+        broken_links1 = self.check_broken_links(driver1)
+        broken_links2 = self.check_broken_links(driver2)
+        print("Broken links in Website 1:", broken_links1)
+        print("Broken links in Website 2:", broken_links2)
 
-    with open(file2, "r") as file:
-        urls2 = set(line.strip() for line in file.readlines())
+        # Analyze page load times
+        self.analyze_page_load_times(driver1, url1)
+        self.analyze_page_load_times(driver2, url2)
 
-    missing_urls = urls1 - urls2
+        driver1.get(url1)
+        driver2.get(url2)
 
-    if missing_urls:
-        print("The following URLs are missing in website2_page_urls.txt:")
-        for url in missing_urls:
-            print(url)
-    else:
-        print("All URLs from website1_page_urls.txt are present in website2_page_urls.txt.")
-
-
-def compare_websites(url1, url2):
-    # Validate URLs
-    if not validate_url(url1) or not validate_url(url2):
-        print("Invalid URL(s).")
-        return
-
-    if url1 == url2:
-        print("Please enter two different URLs for website 1 and website 2.")
-        return
-
-    # Get data for website 1
-    print("Getting data for website 1...")
-    response1 = requests.get(url1)
-
-    if response1.status_code != 200:
-        print("Error: Website 1 could not be accessed.")
-        return
-
-    website1_data = response1.text
-
-    # Get data for website 2
-    print("Getting data for website 2...")
-    response2 = requests.get(url2)
-
-    if response2.status_code != 200:
-        print("Error: Website 2 could not be accessed.")
-        return
-
-    website2_data = response2.text
-
-    # Set up Selenium WebDriver
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    driver1 = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver2 = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-    # Take full page screenshots
-    print("Taking screenshots...")
-    get_full_page_screenshot(driver1, "screenshot1.png")
-    print("Screenshot 1 saved.")
-    get_full_page_screenshot(driver2, "screenshot2.png")
-    print("Screenshot 2 saved.")
-
-    driver1.quit()
-    driver2.quit()
-
-    # Content Comparison (using BeautifulSoup and HtmlDiff)
-    print("\nContent Comparison:")
-
-    if website1_data == website2_data:
-        print("The websites have identical content.")
-    else:
-        print("The websites have different content.")
-
-        # Automated Testing (using BeautifulSoup and HtmlDiff)
-        print("\nAutomated Testing:")
-
-        soup1 = BeautifulSoup(website1_data, "html.parser")
-        soup2 = BeautifulSoup(website2_data, "html.parser")
-
-        # Find all links and check if they are clickable
-        links1 = soup1.find_all("a")
-        links2 = soup2.find_all("a")
-
-        for link in links1:
-            if not link.has_attr("href"):
-                print("Link not clickable on Website 1: ", link)
-
-        for link in links2:
-            if not link.has_attr("href"):
-                print("Link not clickable on Website 2: ", link)
-
-        # Find all buttons and check if they are clickable
-        buttons1 = soup1.find_all("button")
-        buttons2 = soup2.find_all("button")
-
-        for button in buttons1:
-            if not button.has_attr("onclick"):
-                print("Button not clickable on Website 1: ", button)
-
-        for button in buttons2:
-            if not button.has_attr("onclick"):
-                print("Button not clickable on Website 2: ", button)
-
-        # Find all images and check if they are visible
-        images1 = soup1.find_all("img")
-        images2 = soup2.find_all("img")
-
-        for image in images1:
-            if not image.has_attr("src"):
-                print("Image not seen on Website 1: ", image)
-
-        for image in images2:
-            if not image.has_attr("src"):
-                print("Image not seen on Website 2: ", image)
-
-        # Full Differences Comparison
-        print("\nFull Differences Comparison:")
-
+        # Check for differences in page content
         diff = HtmlDiff()
-        diff_output = diff.make_file(website1_data.splitlines(), website2_data.splitlines())
+        website1_html = driver1.page_source
+        website2_html = driver2.page_source
+        content_diff = diff.make_file(website1_html.splitlines(), website2_html.splitlines())
+        with open("content_diff.html", "w", encoding="utf-8") as file:
+            file.write(content_diff)
+        print("Content differences saved to content_diff.html.")
 
-        with open("full_differences.html", "w", encoding="utf-8") as file:
-            file.write(diff_output)
-        print("Full differences saved to full_differences.html.")
+        # Check for visual differences using screenshots
+        driver1.save_screenshot("screenshot1.png")
+        driver2.save_screenshot("screenshot2.png")
+
+        image1 = Image.open("screenshot1.png")
+        image2 = Image.open("screenshot2.png")
+
+        diff_image = ImageChops.difference(image1, image2)
+        if diff_image.getbbox():
+            diff_image.save("visual_diff.png")
+            print("Visual differences saved to visual_diff.png.")
+        else:
+            print("No visual differences.")
 
         # Compare page URLs
-        print("\nComparing Page URLs:")
-        website1_page_urls = get_page_urls(url1)
-        website2_page_urls = get_page_urls(url2)
-        save_page_urls(url1, website1_page_urls, "website1_page_urls.txt")
-        save_page_urls(url2, website2_page_urls, "website2_page_urls.txt")
-        compare_page_urls("website1_page_urls.txt", "website2_page_urls.txt")
+        website1_page_urls = self.get_page_urls(url1)
+        website2_page_urls = self.get_page_urls(url2)
+        self.save_page_urls(url1, website1_page_urls, "website1_page_urls.txt")
+        self.save_page_urls(url2, website2_page_urls, "website2_page_urls.txt")
+
+        driver1.quit()
+        driver2.quit()
+
+    def save_page_urls(self, url, page_urls, filename):
+        with open(filename, "w") as file:
+            file.write(f"Page URLs for {url}:\n")
+            for page_url in page_urls:
+                file.write(page_url + "\n")
+        print(f"Page URLs saved to {filename}.")
 
 
 if __name__ == "__main__":
+    comparer = WebsiteComparer()
     url1 = input("Enter the first website URL: ")
     url2 = input("Enter the second website URL: ")
-
-    compare_websites(url1, url2)
+    comparer.compare_websites(url1, url2)
